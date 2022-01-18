e243fa577288fca962334bc720a6fa8a8ebdfa41
Vatsal-wire <97349629+Vatsal-wire@users.noreply.github.com>
2022-01-18 13:22:08 +0530

    Added various cogs

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import discord
 from discord.ext import commands
-from Cogs import mod as mod
+from Cogs import Moderation
+from Moderation import mod as mod
+
+from Cogs import Music
+from Music import music as music
+
+from Cogs import Fun
+from Music import fun as fun
 
 intents = discord.Intents.all() #need to enable intents
 bot = commands.Bot(command_prefix='~', intents=intents)
 
-cogs1 = [mod]
+cogs1 = [mod, music, fun]
 
 for i in range(len(cogs1)):
     cogs1[i].setup(bot)
