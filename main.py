6877280e60017af3e626f5088dc2a53398dcbb85
pixelator <97349629+Vatsal-wire@users.noreply.github.com>
2022-01-18 19:53:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ from Cogs.Moderation import mod
 from Cogs.Music import music
 from Cogs.Paginator import Paginator
 from Cogs.tictactoe import tic_tac_toe
+from Cogs.Meme import meme
 
 intents = discord.Intents.all() #need to enable intents in discord dev portal
 bot = commands.Bot(command_prefix='~', intents=intents)
 
 
-cogs1 = [fun, Info, mod, music, Paginator, tic_tac_toe]
+cogs1 = [fun, Info, mod, music, Paginator, tic_tac_toe, meme]
 
 for i in range(len(cogs1)):
     cogs1[i].setup(bot)
