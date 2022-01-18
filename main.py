9510b400c80d2189f6c7c3cc3074606b9b0d6bbd
Pogrammar <97349629+pogrammar@users.noreply.github.com>
2022-01-18 22:04:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import discord
 from discord.ext import commands
+import os
 
 
-from Cogs.Fun import fun
-from Cogs.Info import Info
-from Cogs.Moderation import mod
-from Cogs.Music import music
-from Cogs.Paginator import Paginator
-from Cogs.tictactoe import tic_tac_toe
-from Cogs.Meme import meme
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+      bot.load_extension(f'cogs.{filename[:-3]}')
 
 intents = discord.Intents.all() #need to enable intents in discord dev portal
 bot = commands.Bot(command_prefix='~', intents=intents)
-
-
-cogs1 = [fun, Info, mod, music, Paginator, tic_tac_toe, meme]
-
-for i in range(len(cogs1)):
-    cogs1[i].setup(bot)
 
 bot.run('token')        
