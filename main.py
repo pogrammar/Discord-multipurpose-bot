1f16ab8aabf1511fc4a7aaa43cedccea7211f713
Pogrammar <97349629+pogrammar@users.noreply.github.com>
2022-01-19 17:44:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from discord.ext import commands
 import os
 
 bot = commands.Bot(command_prefix='~', intents=intents)
+intents = discord.Intents.all() #need to enable intents in discord dev portal
 
 for foldername in os.listdir('./cogs'):
     for filename in os.listdir(foldername):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.{foldername}{filename[:-3]}')
 
-intents = discord.Intents.all() #need to enable intents in discord dev portal
+
 
 bot.run('token')        
