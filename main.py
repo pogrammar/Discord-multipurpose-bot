6f3c135ee80de0fe9fdb12367f79e9b5836eb904
Pogrammar <97349629+pogrammar@users.noreply.github.com>
2022-01-19 10:59:43 +0530

    Fixed error in main.py
    I had to fix the error where iI just gave it the path of the cogs folder and to load the extension ending with .py, however each cog is in  a subfolder, so i had to make a for loop inside a for loop.
    
    That was a dumb error

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from discord.ext import commands
 import os
 
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-      bot.load_extension(f'cogs.{filename[:-3]}')
+for foldername in os.listdir('./cogs'):
+    for filename in os.listdir(foldername):
+        if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
 intents = discord.Intents.all() #need to enable intents in discord dev portal
 bot = commands.Bot(command_prefix='~', intents=intents)
