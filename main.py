b7c2db8395df9a3d8554c16c728cf5501d8d8bdf
Pogrammar <97349629+pogrammar@users.noreply.github.com>
2022-01-20 11:08:52 +0530

    Added Search cog 🎉

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import os
 intents = discord.Intents.all() #need to enable
 bot = commands.Bot(command_prefix='~', intents=intents)
 
-for foldername in os.listdir('./cogs'):
-    for filename in os.listdir(foldername):
-        if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{foldername}{filename[:-3]}')
+for foldername in os.listdir('./cogs'): #for every folder in cogs
+    for filename in os.listdir(foldername):# for every file in a folder in cogs
+        if filename.endswith('.py'): #if the file is a python file
+            bot.load_extension(f'cogs.{foldername}{filename[:-3]}')#load the extension
 
 
 
