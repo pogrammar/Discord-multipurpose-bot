2b76be96df4fed3dd2f6e13fd377aca624258fe4
Satwik260908 <70678063+Satwik260908@users.noreply.github.com>
2022-01-21 09:18:54 +0530

    Update mod.py

--- Cogs/Moderation/mod.py
+++ Cogs/Moderation/mod.py
@@ -128,6 +128,22 @@ class Moderation(commands.Cog):
         warns = users[str(user.id)][mode]
     
         return warns	
-
+#This is the announce function which allows the user with admin rights to announce something
+	@slash_command(guild_ids=[...])   
+	@has_permissions(administrator = True)
+	async def announce(ctx , 
+		Title : Option(str),
+		Description : Option(str),
+		Color-Hex : Option(str),
+		Thumbnail-Url : Option(str),	
+		Image-Url : Option(str),
+		Footer : Option(str) ) :
+		#function starts
+		 embed = discord.Embed(title=Title, description=Description,color = Color-Hex)
+		 embed.set_image(url=Image-Url)	
+    		 embed.set_thumbnail(Thumbnail-Url)
+   	 	 embed.set_footer(name=Footer) 
+   		 ctx.send(embed=embed)
+   
 def setup(bot):
     bot.add_cog(Moderation(bot))
