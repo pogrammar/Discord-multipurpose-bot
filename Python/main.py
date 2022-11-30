39139d75ac8048d9ff8c1c69c0ee1c230fed5b32
Pogrammar <97349629+pogrammar@users.noreply.github.com>
2022-12-01 00:12:42 +0530

    Fixed issue of wrong file name reference

--- Python/main.py
+++ Python/main.py
@@ -6,10 +6,10 @@ TOKEN = os.environ['TOKEN']
 intents = discord.Intents.all() #need to enable
 bot = commands.Bot(command_prefix='~', intents=intents)
 
-for foldername in os.listdir('./cogs'): #for every folder in cogs
-    for filename in os.listdir(f'./cogs/{foldername}'):# for every file in a folder in cogs
+for foldername in os.listdir('./Cogs'): #for every folder in cogs
+    for filename in os.listdir(f'./Cogs/{foldername}'):# for every file in a folder in cogs
         if filename.endswith('.py') and not filename in ['util.py', 'error.py']: #if the file is a python file and if the file is a cog
-            bot.load_extension(f'cogs.{foldername}.{filename[:-3]}')#load the extension
+            bot.load_extension(f'Cogs.{foldername}.{filename[:-3]}')#load the extension
 
 
 
